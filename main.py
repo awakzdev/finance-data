cf17ff984549f29865bae172f8471506fde4b8f9
eliezerRevach <51950807+eliezerRevach@users.noreply.github.com>
2025-04-08 12:22:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def main():
     today_date = datetime.now().strftime('%Y-%m-%d')
     
     # Symbols to process
-    symbols = ['QLD', '^NDX','SSO','UWM','TQQQ','UPRO','TNA','SPYU','MSTU','NVDL']
+    symbols = ['QLD', '^NDX','SSO','UWM','TQQQ','UPRO','TNA','SPYU','MSTU','NVDL','PLTU','PTIR']
     
     for symbol in symbols:
         try:
